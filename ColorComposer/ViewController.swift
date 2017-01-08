5d966c64d1675c43013534c99d522c1d855e0864
a <ruan65@gmail.com>
2017-01-08 21:03:39 +0300

    first one

--- ColorComposer/ViewController.swift
+++ ColorComposer/ViewController.swift
@@ -17,7 +17,8 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        print("The app has got the memory warninig!!!")
     }
 
 
